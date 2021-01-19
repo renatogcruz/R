ccdd7a18dc3694e17bc6a00074e85b4980bdee1c
renatogcruz <renatogcruz@hotmail.com>
2021-01-19 12:41:35 -0300

    R - tipos de dados

---
+++ Introducao_da_linguagem_R_com_RStudio/dados/tipos_de_dados.R
@@ -0,0 +1,23 @@
+delta <- 8
+
+delta
+
+class(delta)
+
+logico <- TRUE
+logico <- F
+caractere <- "Texto"
+caractere <- 'Texto'
+
+c = 2
+d = 4
+(c + d) * d
+e = (c + d) * d
+e
+
+a = 1
+b = 2
+a < b
+a == b
+
+sqrt(2500)
