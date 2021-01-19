ddb402431635a409a685911d9b86dbe405447917
renatogcruz <renatogcruz@hotmail.com>
2021-01-19 11:44:56 -0300

    testando código - aula aspectos diversos

--- Introducao_da_linguagem_R_com_RStudio/dados/aspectos_diversos.R
+++ Introducao_da_linguagem_R_com_RStudio/dados/aspectos_diversos.R
@@ -1,2 +1,25 @@
+# Saber o diretório padrão
 getwd()
+
+# mudar o diretório padrão
+setwd("C:\\Users\\Renato\\OneDrive\\github\\R\\Introducao_da_linguagem_R_com_RStudio\\dados")
+
+# Informa o tipo do objeto
+class(iris)
+
+# Copiando iris
+iristeste = iris
+# salvando iristeste, cópia do iris
+save(iristeste, file="iristeste.RData")
+# remover o iristeste para testar o carregamento
+rm(iristeste)
+# testando a recupareção
+iristeste
+# carregando arquivo
+load(file="iristeste.Rdata")
+# testando novamente
+iristeste
+
+
+
 
