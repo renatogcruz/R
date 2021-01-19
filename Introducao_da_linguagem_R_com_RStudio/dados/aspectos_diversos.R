2d103a5d81fc41c877a33f0241679493863b25b8
renatogcruz <renatogcruz@hotmail.com>
2021-01-19 11:56:07 -0300

    r - vizualizando dados

--- Introducao_da_linguagem_R_com_RStudio/dados/aspectos_diversos.R
+++ Introducao_da_linguagem_R_com_RStudio/dados/aspectos_diversos.R
@@ -20,6 +20,12 @@ load(file="iristeste.Rdata")
 # testando novamente
 iristeste
 
+# função genérica de impressão
+x = c(12, 34, 56, 79)
+y = c(1, 6, 9, 14)
+plot(x,y)
 
+# quick encerra a sessão
+quit()
 
 
