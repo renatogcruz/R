d86c5c5e0adcb6ac306befb4c9b6c6688406cad3
renatogcruz <renatogcruz@hotmail.com>
2021-03-02 09:58:50 -0300

    R anova de uma vida com medidas repetidas

--- Curso_estatistica_aplicada_no_R/aula_12_ANOVA_uma_via_com_medidas_repetidas.R
+++ Curso_estatistica_aplicada_no_R/aula_12_ANOVA_uma_via_com_medidas_repetidas.R
@@ -1,77 +1,94 @@
-######################### Teste de Kruskal-Wallis #########################
+##################### ANOVA de uma via com medidas repetidas ####################
 
 
 # Passo 1: Carregar os pacotes que serão usados
 
-if(!require(dplyr)) install.packages("dplyr") # Instalação do pacote caso não esteja instalado
-library(dplyr)                                # Carregamento do pacote
-if(!require(rstatix)) install.packages("rstatix") # Instalação do pacote caso não esteja instalado
-library(rstatix)                                  # Carregamento do pacote
-if(!require(ggplot2)) install.packages("ggplot2") # Instalação do pacote caso não esteja instalado
-library(ggplot2)                                  # Carregamento do pacote
+if(!require(dplyr)) install.packages("dplyr")
+library(dplyr)                                
+if(!require(ez)) install.packages("ez") 
+library(ez)
+if(!require(reshape)) install.packages("reshape") 
+library(reshape) 
+if(!require(rstatix)) install.packages("rstatix") 
+library(rstatix)
 
 # Passo 2: Carregar o banco de dados
 
 # Importante: selecionar o diretório de trabalho (working directory)
 # Isso pode ser feito manualmente: Session > Set Working Directory > Choose Directory
-# Ou usando a linha de código abaixo:
-# setwd("C:/Users/ferna/Desktop")
 
-dados <- read.csv('Banco de Dados 5.csv', sep = ';', dec = ',') # Carregamento do arquivo csv
-View(dados)                                       # Visualização dos dados em janela separada
-glimpse(dados)                                          # Visualização de um resumo dos dados
-
-
-# Passo 3: Realização do teste de Kruskal-Wallis
-
-kruskal.test(BC ~ Grupo, data = dados)
-kruskal.test(Pressao ~ Grupo, data = dados)
-
-
-# Passo 4: Testes de post-hoc
-
-# Teste de Dunn com ajuste do valor de p
-dunn_test(BC ~ Grupo, data = dados, p.adjust.method = "bonferroni")
-dunn_test(Pressao ~ Grupo, data = dados, p.adjust.method = "bonferroni")
-
-
-# Passo 5: Análise descritiva dos dados
-dados %>% group_by(Grupo) %>% 
-  get_summary_stats(BC, Pressao, type = "median_iqr")
-
-
-# Passo 6: Visualização dos dados
-par(mfrow=c(1,2))
-boxplot(BC ~ Grupo, data = dados)
-boxplot(Pressao ~ Grupo, data = dados)
-
-
-# Passo 7: Análise da distribuição
-
-par(mfrow=c(1,3))
-hist(dados$BC[dados$Grupo == "Placebo"],
-     ylab = "Frequência", xlab = "bps", main="Placebo")
-hist(dados$BC[dados$Grupo == "AH Novo"],
-     ylab = "Frequência", xlab = "bps", main="AH Novo")
-hist(dados$BC[dados$Grupo == "AH Padrão"],
-     ylab = "Frequência", xlab = "bps", main="AH Padrão")
+dados <- read.csv2('Banco de Dados 7.csv') # Carregamento do arquivo csv
+View(dados)                                # Visualização dos dados em janela separada
+glimpse(dados)                             # Visualização de um resumo dos dados
 
 
 
-par(mfrow=c(1,3))
-hist(dados$Pressao[dados$Grupo == "Placebo"],
-     ylab="Frequência", xlab="bps", main="Placebo")
-hist(dados$Pressao[dados$Grupo == "AH Novo"],
-     ylab="Frequência", xlab="bps", main="AH Novo")
-hist(dados$Pressao[dados$Grupo == "AH Padrão"],
-     ylab="Frequência", xlab="bps", main="AH Padrão")
+# Passo 3: Alterar o formato do banco de dados de "wide" para "long" (pacote: reshape)
+
+# Reestruturando o banco de dados
+
+dadosl <- melt(dados,
+               id = "ID",
+               measured = c("Professor1", "Professor2", "Professor3", "Professor4"))
+
+View(dadosl)
+
+# Renomeando as colunas do novo banco
+colnames(dadosl) = c("ID", "Professor", "Nota")
+
+# Ordenando as colunas pelo sujeito experimental
+dadosl <- sort_df(dadosl, vars = "ID")
 
 
-# Histograma com todos os grupos, separados por cor
-ggplot(dados, aes(x = BC)) +
-  geom_histogram(aes(color = Grupo, fill = Grupo),
-                 alpha = 0.3, position = "identity", binwidth = 10)
+glimpse(dadosl)
 
-ggplot(dados, aes(x = Pressao)) +
-  geom_histogram(aes(color = Grupo, fill = Grupo),
-                 alpha = 0.3, position = "dodge", binwidth = 10)
+# Transformando a variável ID em fator
+dadosl$ID <- factor(dadosl$ID)
+
+
+
+# Passo 4: Checar os pressupostos de normalidade e ausência de outliers (pacote: rstatix)
+
+# Verificando a presença de outliers por grupo
+dadosl %>% group_by(Professor) %>% 
+  identify_outliers(Nota)
+
+
+# Verificando a normalidade por grupo
+dadosl %>% group_by(Professor) %>% 
+  shapiro_test(Nota)
+
+
+# Passo 5: Construção do modelo da ANOVA com medidas repetidas (pacote: ez)
+
+mod.ANOVA <- ezANOVA(data = dadosl,
+                     dv = Nota,
+                     wid = ID,
+                     within = Professor,
+                     detailed = TRUE,
+                     type = 3)
+
+# dv = variável dependente
+# wid = variável de identificação do sujeito
+# within = variável independente de medidas repetidas
+# type = tipo da soma dos quadrados (default é o tipo II, tipo III é o padrão no SPSS)
+
+
+# Passo 5: Analisando os resultados do modelo
+mod.ANOVA
+
+# Obs.: Libera o teste de esfericidade de Mauchly, e as correções
+#       de Greenhouse-Geisser e Huynh-Feldt
+
+
+# Passo 6: Testes de post-hoc
+pairwise.t.test(dadosl$Nota, dadosl$Professor, paired = TRUE,
+                p.adjust.method = "bonferroni")
+
+
+
+# Passo 7 (opcional): Análise descritiva dos dados (pacote: rstatix)
+dadosl %>% group_by(Professor) %>% 
+  get_summary_stats(Nota, type = "mean_sd")
+
+boxplot(Nota ~ Professor, data = dadosl, ylab = "Notas", xlab = "Professor")
