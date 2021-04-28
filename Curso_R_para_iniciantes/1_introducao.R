e04727f9e9724dec40b38c38f3fa5a6cbd63a2eb
renatogcruz <renatogcruz@hotmail.com>
2021-04-28 09:58:05 -0300

    atribuição de variaiveis

---
+++ Curso_R_para_iniciantes/1_introducao.R
@@ -0,0 +1,62 @@
+# Objetivo: primeiro contato com a linguagem R (segunda, terceira, quarta...)
+
+# craindo variaveis
+# para executar clique sobre a linha ou selecione o conjunto de linhas
+# e precione ctrl + enter
+
+a <- 10 # em R, atribui-se variável com '<-' (melhor forma de atribuir) ou '='
+a
+
+b
+b = 10
+b
+B
+
+# utilizaremos <- na criacao de variaveis e = nas atribuições de funcoes
+c -> 10
+10 -> c
+c -> d
+
+# para padrozinar sempre utilizaremos 'varaiveis' <- 'valor'
+a <- 10
+b <- 5
+
+a
+b
+
+c <- a + b
+c
+
+a <- 'Heitor'
+b <- 'Joao'
+
+a
+b
+
+c <- a + b
+c
+
+# Funções
+# fazem parte de um pacote
+# pacotes contem muitas funcoes e precisam estar instalados
+# não sendo um pacote padrao ele precisa ser chamado
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
