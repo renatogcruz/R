e05405426e3bcca99c16e554d7ab5ed143b11cdf
renatogcruz <renatogcruz@hotmail.com>
2021-04-29 08:30:52 -0300

    aula operadores em R

---
+++ Curso_R_para_iniciantes/2_operadores.R
@@ -0,0 +1,63 @@
+# Objetivo: realizar operações matemáticas, logicas e de comparação
+
+# Como?
+# utilizando recursos básicos
+
+# soma
+5 + 5
+
+# subtração
+10 - 8
+
+# divisão
+9 / 3
+
+# multiplicação
+4 * 2
+
+# potencia
+4 ** 2 # igual Python
+4 ^ 2
+
+# igual?
+5 == 6
+5 == 5
+
+# diferente?
+5 != 6
+5 != 5
+
+# maior?
+4 > 2
+4 > 6
+
+# menor?
+4 < 2
+4 < 6
+
+# maior ou igual?
+4 >= 2
+4 >= 4
+
+# menor ou igual?
+4 <= 6
+4 <= 4
+
+# E
+6 == 6
+6 == 6 & 7 == 8   #parecido com C# (&&)
+6 == 6 & 7 != 8
+
+# Ou
+6 == 6
+6 == 6 | 7 == 8   #parecido com C# (||)
+6 == 6 | 7 != 8
+
+# negação
+!6 == 6           #parecido com C#, em Python usa-se NOT
+!4 > 2
+
+
+
+
+
