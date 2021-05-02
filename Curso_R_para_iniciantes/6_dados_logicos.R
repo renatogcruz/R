630cca303d5007caa9db4a673a099f97c08eddc3
renatogcruz <renatogcruz@hotmail.com>
2021-05-02 08:26:45 -0300

    dados lógicos com R

---
+++ Curso_R_para_iniciantes/6_dados_logicos.R
@@ -0,0 +1,13 @@
+# armazenamento lógico
+
+salario <- 8816.25
+hora <- 20
+
+l1 <- salario > hora
+l1
+
+logico <- TRUE         # plavra reservada TRUE
+logico1 <- "TRUE"      # string
+
+logico2 <- c(1, TRUE, FALSE, 3)
+logico2
