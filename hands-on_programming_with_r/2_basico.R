99f3755f2e7e94a6f93993b797dbf280c01c9317
renatogcruz <renatogcruz@hotmail.com>
2021-05-26 10:28:24 -0300

    add hands-on with r

---
+++ hands-on_programming_with_r/2_basico.R
@@ -0,0 +1,58 @@
+1:2
+1:4
+
+die <- 1:6
+die
+
+die + 1:2
+die + 1:4
+
+die %*% die #multiplicação interna com %*%
+
+die %o% die
+
+
+# FUNÇÕES
+
+round(3.141516)
+
+factorial(3)
+
+mean(1:6)
+
+mean(die)
+
+round(mean(die))
+
+# FUNÇÃO sample (Random Samples and Permutations)
+
+?sample      #ajuda (retorna descrição e ajuda da função)
+args(sample) #ajuda rápida (retorna a estrutura da função)
+
+# sample é uma boa função para rolarmos o dado
+
+sample(x = 1:4, size = 2)
+
+# para rolar o dado e obter um número de volta
+sample(x = die, size = 1)
+
+sample(x = die, size = 1)
+
+sample(die, 1) #maneira economica de se escrever
+
+
+# 2.3.1 Amostra com Substituição
+
+sample(die, size = 2) #pode parecer que simula 2 dados, mas o primeiro valor
+                      #nuca será igual ao primeiro. Cuidado
+sample(die, size = 2, replace = TRUE) #replace TRUE corrige este erro
+
+args(sample) #revendo os inputs da função sample
+
+dados <- sample(die, size = 2, replace = TRUE)
+dados
+
+sum(dados)
+
+# 2.4 Escrevendo suas próprias funções
+
