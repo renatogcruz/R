5f57878c72e5edd000e32375bc3496d09bd7d84a
renatogcruz <renatogcruz@hotmail.com>
2021-05-31 10:46:43 -0300

    tipos de variaveis em R

--- hands-on_programming_with_r/5_objetos.R
+++ hands-on_programming_with_r/5_objetos.R
@@ -30,11 +30,64 @@ text <- c("ace", "hearts")
 sum(int)
 sum(text) # contém um erro
 
-# 5.1.1 Duplas
+# 5.1.1 double
+die <- c(1, 2, 3, 4, 5, 6)
+die
+typeof(die) # que tipo de objeto é die??
+
+# 5.1.2 Inteiros
+int <- c(1L, 2L, 4L) # observe que o R não salvará um número inteiro, 
+                     # ao menos que você inclua o L
+int
+typeof(int)
+
+# por que vc salvaria seus dados como um ineiro em vez de um double?
+# às vezes, uma diferença na precisão pode ter efeitos surpreendentes.
+
+# Ex.: as vezes vc espera que o resultado abaixo seja zero, mas não é:
+sqrt(2)^2 - 2
+
+# esses erros são conhecidos como erros de PONTOS FLUTUANTES
+
+# 5.1.3 Characters
+text <- c("Hello", "World")
+text
+typeof(text)
+typeof("Hello")
+
+# 5.1.4 - lógica
+
+3 > 4
+logic <- c(TRUE, FALSE, TRUE)
+logic
+typeof(logic)
+
+# 5.1.5 Complexo e Bruto
+
+# vetores complexos armazenam números complexos. Para criar um vetor
+# complexo, adicione um termo imaginário a um número com "i"
+
+comp <- c(1 + 1i + 1 + 2i + 1 + 3i)
+comp
+typeof(comp)
+
+# vetores brutos armazenam bytes de dados brutos. Tornar vetores brutos fica
+# complicado, mas você pode fazer um vetor bruto vazio de comprimento n com
+# raw(n)
+?raw
 
 
+raw(3)
+typeof(raw(3))
 
+# Exercício 5.2 (vetor de cartas)
 
+hand <- c("ace", "king", "queen", "jack", "ten")
+hand
+
+typeof(hand)
+
+# 5.2 Atributos
 
 
 
