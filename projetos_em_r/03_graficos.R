6b367fd1f78b5e563e500483d6335bb6fd78fc4d
renatogcruz <renatogcruz@hotmail.com>
2021-11-02 08:57:52 -0300

    studying the ggplot2 library

--- projetos_em_r/03_graficos.R
+++ projetos_em_r/03_graficos.R
@@ -130,19 +130,65 @@ boxplot(Temp ~ Month, data=airquality, xaxt="n")
 axis(1, at=1:5, labels=c("May", "June", "July", "August", "September"))
 
 # EVOLUINDO PARA O GGPLOT2 - página 55
+ggplot(airquality, aes(x=Temp))+geom_histogram()
+
+# estrutura geral do ggplot
+#ggplot(data_source, aes(componentes de dados do mapa para componentes gráficos)) +
+#geom_xxx(argumentos para modificar a aparência de geom) +
+#theme_xxx(argumentos para alterar a aparência geral) +
+#labs(adicionar rótulos do eixo e um título
+
+# HISTOGRAMAS
+ggplot(airquality, aes(x=Temp)) + 
+        geom_histogram(binwidth = 5, color = "black", fill="grey80") + 
+        theme_bw() + 
+        labs(x = "Temperature (Fahrenheit)", y = "Frequency", title=
+                     "Temperatures in the airquality Data Frame")
+
+# GRÁFICOS DE BARRAS
+library(MASS)
+ggplot(Cars93, aes(x=Type)) +
+        geom_bar() +
+        labs(y = "Frequency", title = "Car Type and Frequency in Car93")
+
+# FRÁFICOS DE BARRAS AGRUPADOS
+Type.Origin <- subset(Cars93, select=c("Type", "Origin"))
+
+head(Type.Origin)
+ggplot(Type.Origin, aes(x = Type, fill=Origin)) +
+        geom_bar(position = "dodge", color = "black") +
+        scale_fill_grey(start = 0, end = 1)
+
+# agrupando mais uma vez
+females.df <- data.frame(females)
+females.df
+
+ggplot(females.df, aes(x = Hair, y = Freq, fill = Eye)) +
+        geom_bar(position = "dodge", color = "black") +
+        scale_fill_grey(start = 0, end = 1)
+
+# GRÁFICOS DE DISPERSÃO
+
+ggplot(airquality, aes(x = Wind, y = Temp)) +
+        geom_point()
+
+# usando a função drop.na() do tidyr para eliminar todos os valores NA:
+library(tidyr)
+aq.no.NA <- drop_na(airquality)
+median.Ozone <- median(aq.no.NA$Ozone)
+
+Ozone_level <- NULL
+
+for(i in 1: nrow(aq.no.NA)){
+        if (aq.no.NA$Ozone[i]<= median.Ozone){
+                Ozone_level[i] <- "Low"}
+        else {Ozone_level[i] <- "High"}}
+aq.no.NA <- cbind(aq.no.NA, Ozone_level)
 
 
+# MATRIZ DE GRÁFICO DE DISPERSÃO - página 69
 
-
-
-
-
-
-
-
-
-
-
+# DIAGRAMA DE CAIXAS - página 71
 
 
 
